8dd46bc3363ada5ffe7a7acd470950f3487568b1
jonasgossiaux <jonasgossiaux@hotmail.com>
2016-03-23 11:43:34 +0100

    added empty test class for shuffle

---
+++ app/src/logic/test/TestShuffle.java
@@ -0,0 +1,37 @@
+package logic.test;
+
+import static org.junit.Assert.*;
+
+import logic.BasicCard;
+import logic.TreasureCard;
+import logic.VictoryCard;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * Created by Jonasty on 23/03/2016.
+ */
+public class TestShuffle {
+
+    public List testDeck;
+
+    @Before
+    public void beforeShuffle(){
+
+
+
+
+    }
+
+
+    @Test
+    public void testShuffle(){
+
+    }
+
+}
