c17e8152d9d13657a25b8172762e62ab9ea7ee0b
casiernick <casiernick@gmail.com>
2016-03-23 12:11:31 +0100

    Porfile info

--- app/src/UserProfile/Profile.java
+++ app/src/UserProfile/Profile.java
@@ -1,9 +1,36 @@
 package UserProfile;
 
+import java.util.Date;
+
 /**
  * Created by Nick Casier on 23/03/16.
  */
 public class Profile {
+    private String name;
+    private String mail;
+    private String password;
+    private Date birthDate;
+    private int gameID; //for the moment String, will change
 
+    public String getName(){
+        return name;
+    }
+
+    public String getMail(){
+        return mail;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public Date getBirthDate(){
+        return birthDate;
+    }
+
+    public int getGameID()
+    {
+        return gameID;
+    }
 
 }
