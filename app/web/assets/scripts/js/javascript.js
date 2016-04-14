6ce34920669dc19fe051ca50be1aa771047c8b27
Indy Van Mol <indyvanmol@icloud.com>
2016-04-14 16:24:21 +0200

    Basic game start form.

--- app/web/assets/scripts/js/javascript.js
+++ app/web/assets/scripts/js/javascript.js
@@ -1,3 +1,9 @@
+function definePlayerArray() {
+    var playerArray = [$("#player1").val(), $("#player2").val()]
+    console.log(playerArray);
+    $('#playerForm').hide();
+}
+
 $(document).ready(function () {
-    
+    $('#playerForm').on('submit', definePlayerArray);
 });
