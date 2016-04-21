fdad27cd88735c9da91f588d2f4f338c4ec05eba
Jasperbl <jasper.blontrock@student.howest.be>
2016-04-21 11:52:58 +0200

    no message

--- app/src/Console/ConsoleReadingDemo.java
+++ app/src/Console/ConsoleReadingDemo.java
@@ -1,5 +1,7 @@
 package Console;
 
+import logic.GameEngine;
+
 import java.util.Scanner;
 
 /**
@@ -11,11 +13,14 @@ public class ConsoleReadingDemo {
         Scanner in = new Scanner(System.in);
         //int i = in.nextInt();
         System.out.print("Name player1: ");
-        String s1 = in.next();
+        String player1 = in.next();
         System.out.print("Name player2: ");
-        String s2 = in.next();
+        String player2 = in.next();
         System.out.println();
-        System.out.println("The players are: "+ s1 + " and "+ s2);
+        System.out.println("The players are: "+ player1 + " and "+ player2);
+        GameEngine game = new gameLogic(player1, player2);
+        
+
 
 
     }
