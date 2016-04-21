1be90d338c82711f7ef309468ec0553e51269b38
Indy Van Mol <indyvanmol@icloud.com>
2016-04-21 09:44:00 +0200

    GameEngine has a basic constructor to create players.

--- app/src/logic/GameEngine.java
+++ app/src/logic/GameEngine.java
@@ -6,10 +6,12 @@ package logic;
 
 
 public class GameEngine {
+    TreasureCard copper = new TreasureCard(0, 1);
+    VictoryCard estate = new VictoryCard(2, 1);
     private Player[] players;
 
-    public void GameEngine() {
-
+    public void GameEngine(String player1, String player2) {
+        players = new Player[]{new Player(player1, copper, estate), new Player(player2, copper, estate)};
     }
 
 
