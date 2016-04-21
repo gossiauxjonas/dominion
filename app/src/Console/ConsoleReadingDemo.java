70526afb53cb24395629ef8555e182f6614c7e36
Indy Van Mol <indyvanmol@icloud.com>
2016-04-21 10:17:28 +0200

    Added a simple console application so our teammate understands it.

--- app/src/Console/ConsoleReadingDemo.java
+++ app/src/Console/ConsoleReadingDemo.java
@@ -1,34 +1,19 @@
 package Console;
 
+import java.util.Scanner;
+
 /**
  * Created by jasper on 21/04/2016.
  */
-import java.io.Console;
-
 public class ConsoleReadingDemo {
+
     public static void main(String[] args) {
-
-        Console cnsl = null;
-        String name = null;
-
-        try{
-            // creates a console object
-            cnsl = System.console();
-
-            // if console is not null
-            if (cnsl != null) {
-
-                // read line from the user input
-                name = cnsl.readLine("Name: ");
-
-                // prints
-                System.out.println("Name entered : " + name);
-            }
-        }catch(Exception ex){
-
-            // if any error occurs
-            ex.printStackTrace();
-        }
+        Scanner in = new Scanner(System.in);
+        //int i = in.nextInt();
+        System.out.print("Name player1: ");
+        String s = in.next();
+        System.out.println();
+        System.out.println(s);
     }
 
 
