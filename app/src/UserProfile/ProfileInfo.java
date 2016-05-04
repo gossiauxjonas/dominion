7b7986fa7084bb034f44ded3ccfa936d56b21156
Nick Casier <Nick Casier>
2016-05-04 11:43:49 +0200

    Why is the Profile Package still present?

--- app/src/UserProfile/ProfileInfo.java
+++ app/src/UserProfile/ProfileInfo.java
@@ -7,34 +7,4 @@ import java.util.Objects;
  * Created by Nick Casier on 23/03/16.
  */
 public class ProfileInfo {
-    private String name;
-    private String mail;
-    private String password;
-    private Date birthDate;
-    private int gameID; //for the moment int, will probably change
-
-    public String getName() {
-        return name;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public int getGameID() {
-        return gameID;
-    }
-
-
-    public static Object[] profileSetup = {"Nick", "casiernick@gmail.com", "Password1234", "13/10/1996", 55};
-
-
 }
