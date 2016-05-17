369c2eb56d8474035124a33727ba6250c68477f5
Nick Casier <Nick Casier>
2016-05-17 13:50:35 +0200

    All SQL manipulations are now possible. Deleted unnecessary classes and interfaces.

--- app/src/Database/SavedGameDAOInterface.java
+++
@@ -1,8 +0,0 @@
-package Database;
-
-/**
- * Created by Nick Casier on 12/05/2016.
- */
-public interface SavedGameDAOInterface {
-
-}
--- app/src/UserProfile/profileSetup.java
+++
@@ -1,7 +0,0 @@
-package UserProfile;
-
-/**
- * Created by Nick Casier on 24/03/16.
- */
-public class profileSetup {
-}
